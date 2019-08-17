3ecf037fd243ba45b2c4099cf4e1c50037163bc1
Szymon 'SzeN' Hennig <sz.hennig@npc.pl>
2019-08-17 11:18:33 +0200

    add shake heart animation

--- src/app/components/heartsBar/Heart.js
+++ src/app/components/heartsBar/Heart.js
@@ -1,8 +1,40 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import heart from "../../../assets/img/interface/heart.png"
+
+const shakeHeart = keyframes`
+    0% { 
+    left: 0;
+    }
+    24% { 
+    left: 0;
+    }
+    25% { 
+    left: 5px;
+    }
+    49%  { 
+    left: 5px;
+    }
+    50%  { 
+    left: 0px;
+    }
+    74%  { 
+    left: 0px;
+    }
+    75%  { 
+    left: -5px;
+    }
+    99%  { 
+    left: -5px;
+    }
+    100% { 
+    left: 0px;
+    } 
+`
+
 
 export const Heart = styled.div`
 display: inline-flex;
+position: relative;
 background-image: url(${ heart });
 background-size: 100%;
 background-repeat: no-repeat;
@@ -10,4 +42,6 @@ opacity: ${ ( props ) => !props.active ? 1 : 0.5 };
 height: 30px;
 width: 30px;
 margin-right: 5px;
+animation: ${ (props) => props.active ? shakeHeart : null} .3s steps(5) 4;
 `;
+
