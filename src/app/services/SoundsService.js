0d12984375782188561374fd21a6ebb09b6416d8
Szymon 'SzeN' Hennig <sz.hennig@npc.pl>
2019-08-17 12:50:09 +0200

    add sound service

--- src/app/services/SoundsService.js
+++ src/app/services/SoundsService.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React from "react";
+import Sound from 'react-sound';
 
-export class SoundsService {
+class SoundService extends React.PureComponent {
+    state = {
+        isActive: true
+    }
 
+    switchSounds(value) {
+        this.setState((prev) => ({
+            ...prev,
+            isActive: value
+        }))
+    }
+
+    render() {
+        const {isActive} = this.state;
+        return (
+            <Sound loop={true} playStatus={isActive ? 'PLAYING' : 'STOPPED'}/>
+        )
+    }
 }
+
+export const SoundServiceContext = React.createContext();
+
+export {SoundService}
